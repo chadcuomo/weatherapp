6e2b7ca10258374d4f078489784e361c27dd76c7
Chad <chad.cuomo@yahoo.com>
2020-03-16 17:01:27 -0400

    added a function to fetch data from weather api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const body = document.querySelector('body');
+const weatherKey = '1446f31a2dcb4ec692617ff84214db36';
+let userLocation = [];
+let userWeather = [];
+
 
 async function fetchCity() {
     const resp = await fetch('https://ipapi.co/json/');
@@ -6,10 +10,33 @@ async function fetchCity() {
     return ip;
 }
 
+async function storeData() {
+    let location = await fetchCity();
+    userLocation = location;
+    console.log(userLocation);
+}
+
+async function fetchWeather() {
+    await storeData();
+    const resp = await fetch(`https://api.weatherbit.io/v2.0/current?city=${userLocation.city},${userLocation.region}&key=${weatherKey}&units=I`);
+    const weather = await resp.json();
+    return weather;
+}
+
+async function storeWeather() {
+    let currentWeather = await fetchWeather();
+    userWeather = currentWeather.data[0];
+    console.log(userWeather);
+}
+
 async function displayData() {
-    const data = await fetchCity();
-    body.textContent = `${data.city}, ${data.region}`;
-    console.log(data);
+    await storeData();
+    await storeWeather();
+    body.innerHTML = `locaction: ${userLocation.city}, ${userLocation.region}
+    <br> Current temp: ${userWeather.temp}`;
+    
+    
 }
 
 displayData();
+storeWeather();
