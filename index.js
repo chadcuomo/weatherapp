3f6f70c37345f2a44d733942f12869eeffa11465
Chad <chad.cuomo@yahoo.com>
2020-04-01 18:47:11 -0400

    started adding functionality for populating local temperature, location, cloudy, precipitation, humidity, and wind. need to figure out how to populate forecast

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const body = document.querySelector('body');
+const displayTemp = document.querySelector('.temp');
+const displayLocation = document.querySelector('.display-location');
+const displayTime = document.querySelector('.display-time');
+const displayCity = document.querySelector('.city-name');
+const displayCloud = document.querySelector('.cloudy');
+const displayPrecip = document.querySelector('.precip');
+const displayHumidity = document.querySelector('.humidity');
+const displayWind = document.querySelector('.wind');
 const weatherKey = '1446f31a2dcb4ec692617ff84214db36';
 let userLocation = [];
 let userWeather = [];
@@ -32,6 +40,15 @@ async function storeWeather() {
 async function displayData() {
     await storeData();
     await storeWeather();
+    displayTemp.innerHTML = `${userWeather.temp}°`;
+    displayLocation.innerHTML = `${userLocation.city}, ${userLocation.region}`;
+    displayTime.innerHTML = `${userWeather.datetime}`;
+    displayCity.innerHTML = `${userLocation.city}`;
+    displayCloud.innerHTML = `${userWeather.clouds}%`;
+    displayPrecip.innerHTML = `${userWeather.precip} in.`;
+    displayHumidity.innerHTML = `${userWeather.rh}%`
+    displayWind.innerHTML = `${userWeather.wind_spd} mph`
+    
     // body.innerHTML = `locaction: ${userLocation.city}, ${userLocation.region}
     // <br> Current temp: ${userWeather.temp}
     // <br> ${userWeather.weather.icon}`;
